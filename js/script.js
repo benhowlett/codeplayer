a02de3d8a6a7d10e39b4caeaba05c813601c815a
Ben Howlett <ben.howlett@gmail.com>
2016-02-02 21:18:25 -0700

    fixed the javascript, not perfect but it works. kinda.

--- js/script.js
+++ js/script.js
@@ -36,42 +36,9 @@ $(document).ready(function() {
 
     //Display contents of HTML, CSS and JS in iFrame
     $("#run").click(function() {
-        /*
-            //Find body tags in iFrame
-            var frameBody = $("#result").contents().find("body");
 
-            //Add style tags to head
-            $("#result").contents().find("head").append('<style type="text/css"></style>');
+        console.log($("#result").contents().find("html"));
 
-            //Find style tags in iFrame
-            var frameStyle = $("#result").contents().find("style");
-
-            //If there is text in CSS, add it to the iFrame
-            if ($("#css").val() != "") {
-                frameStyle.text($("#css").val());
-            }
-
-            //If there is text in HTML, add it to the iFrame
-            if ($("#html").val() != "") {
-                frameBody.html($("#html").val());
-            }
-
-
-            //If there is text in JS, add it to the end of the body in the iFrame
-            if ($("#js").val() != "") {
-
-                frameBody.append('<script type="text/javascript" src="js/jquery.min.js"><\/script>')
-
-                var scriptTag = '<script type="text/javascript">' + $("#js").val() +'<\/script>';
-
-                frameBody.append(scriptTag);
-
-
-                //body.find("script").text($("#js").val());
-
-                //$("#result").attr("src", function ( i, val ) { return val; });
-            }
-        */
         $("#result").contents().find("html").html("<style>" + $("#css").val() + "</style>" + $("#html").val());
 
         document.getElementById("result").contentWindow.eval($("#js").val());
